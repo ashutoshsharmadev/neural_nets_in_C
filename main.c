3c150afdfab020c3f11344cc0a93008ebb1d4715
ashutoshsharmadev <ashutoshsharma.0499@gmail.com>
2026-01-24 03:45:24 +0530

    better variable names

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ float train[][4] = {
     {3,6},
 };
 
-#define train_count (sizeof(train)/sizeof(train[0]))
+#define TRAINING_SAMPLE_COUNT (sizeof(train)/sizeof(train[0]))
 
 float rand_float(){
     return (float)rand()/(float)RAND_MAX;
@@ -18,14 +18,14 @@ float rand_float(){
 
 float cost(float w){
         float result = 0.0f;
-        for (size_t i=0;i<train_count;i++){
+        for (size_t i=0;i<TRAINING_SAMPLE_COUNT;i++){
         float x = train[i][0];
         float y = x*w;
         // printf("%f,%f\n",y,train[i][1]);
-        float d = y - train[i][1];
-        result += d*d;
+        float error = y - train[i][1];
+        result += error*error;
     }
-    result /= train_count;
+    result /= TRAINING_SAMPLE_COUNT;
     return result;
 }
 
@@ -33,14 +33,16 @@ int main(void){
     // y = x * w;
     // or y = x * w * b;
     srand(56);
+    // w is weight, b is bias
+    // cost function used is mean squared error
     float w = rand_float()*10.0f;
 
     float eps = 1e-3;
     for (size_t i = 0; i < 1000; ++i){
-    float dcost = (cost(w+eps)-cost(w))/eps;
-    float rate = 1e-3;
+    float gradient = (cost(w+eps)-cost(w))/eps;
+    float learning_rate = 1e-3;
     printf("%f\n",cost(w));
-    w -= rate*dcost;
+    w -= learning_rate*gradient;
 }
 printf("%f\n",w);
     // printf("%f\n",cost(w));
