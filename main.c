63aa1a989d649d64fe53fc13e7894b550e6abebe
ashutoshsharmadev <ashutoshsharma.0499@gmail.com>
2026-01-24 03:42:19 +0530

    basic single neuron

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+float train[][4] = {
+    {0,0},
+    {1,2},
+    {2,4},
+    {3,6},
+};
+
+#define train_count (sizeof(train)/sizeof(train[0]))
+
+float rand_float(){
+    return (float)rand()/(float)RAND_MAX;
+}
+
+float cost(float w){
+        float result = 0.0f;
+        for (size_t i=0;i<train_count;i++){
+        float x = train[i][0];
+        float y = x*w;
+        printf("%f,%f\n",y,train[i][1]);
+        float d = y - train[i][1];
+        result += d*d;
+    }
+    result /= train_count;
+    return result;
+}
+
+int main(void){
+    // y = x * w;
+    // or y = x * w * b;
+    srand(56);
+    float w = rand_float()*10.0f;
+
+    
+    printf("%f\n",cost(w));
+  
+    // printf("hello \n");
+    return 0;
+}
